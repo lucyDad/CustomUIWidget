231b1cdcb7f6ca67404d0d2bf6f22c4ef01b7f6f
hexiang <xiang.he@zhenai.com>
2020-11-07 14:27:25 +0800

    添加统一头文件

---
+++ CustomUIWidget/Classes/Public/CustomUIWidget.h
@@ -0,0 +1,43 @@
+//
+//  CustomUIWidget.h
+//  CustomUIWidget
+//
+//  Created by hexiang on 2020/11/7.
+//
+
+#ifndef CustomUIWidget_h
+#define CustomUIWidget_h
+
+#if __has_include(<CustomUIWidget/CustomUIWidget.h>)
+
+#import <CustomUIWidget/FDCommonAlertPresendManager.h>
+#import <CustomUIWidget/FDCommonAlertView.h>
+#import <CustomUIWidget/FDCommonButtonView.h>
+#import <CustomUIWidget/UIView+FDAlertView.h>
+#import <CustomUIWidget/FDAnimationManagerView.h>
+#import <CustomUIWidget/FDArrowTipsView.h>
+#import <CustomUIWidget/FDArrowCustomListView.h>
+#import <CustomUIWidget/UIView+ArrowTipsView.h>
+#import <CustomUIWidget/FDAutoPlaceView.h>
+#import <CustomUIWidget/FDCollectionCellView.h>
+#import <CustomUIWidget/FDCollectionCellViewHelper.h>
+#import <CustomUIWidget/FDMarqueeView.h>
+
+#else
+
+#import "FDCommonAlertPresendManager.h"
+#import "FDCommonAlertView.h"
+#import "FDCommonButtonView.h"
+#import "UIView+FDAlertView.h"
+#import "FDAnimationManagerView.h"
+#import "FDArrowTipsView.h"
+#import "FDArrowCustomListView.h"
+#import "UIView+ArrowTipsView.h"
+#import "FDAutoPlaceView.h"
+#import "FDCollectionCellView.h"
+#import "FDCollectionCellViewHelper.h"
+#import "FDMarqueeView.h"
+
+#endif
+
+#endif /* CustomUIWidget_h */
